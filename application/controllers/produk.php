43f4fd8229bbb5a3fe6781e1925d0d6ab52feafd
dimasaji89 <dimasaji1111@gmail.com>
2025-06-15 17:29:39 +0700

    update produk.php

--- application/controllers/produk.php
+++ application/controllers/produk.php
@@ -40,7 +40,7 @@ class Produk extends MY_Controller {
             $this->session->set_flashdata('success','Produk berhasil disimpan');
             redirect('produk');
         } else {
-            $this->session->set_flashdata('error','Produk gagal disimpan');
+            $this->session->set_flashdata('error','Produk Gagal disimpan');
             redirect('produk');
         }
     }
